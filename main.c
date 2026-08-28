de1416f3840e17f2b7395842d07a4a60dd64596c
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-08-28 16:09:31 -0300

    refactor: criando struct da sessão de recarga

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int id;
+    float energia;
+    float tempo;
+    float custo;
+    float bateria_inicial;
+    float bateria_final;
+    int horario_sessao;
+} Sessao;
+
+int main() {
+    return 0;
+}
