97d276d142eb0bca08b8c765e309ad91484e5b57
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-08-28 16:51:46 -0300

    feat: menu inicial e encerrar programa adicionado

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct
 {
@@ -11,6 +12,80 @@ typedef struct
     int horario_sessao;
 } Sessao;
 
+void exibir_iguais();
+void titulo(char *palavra);
+void menu_inicial();
+void sair_programa();
+
 int main() {
+    int opcao;
+
+    menu_inicial();
+    
+    while (1)
+    {
+        printf("Digite uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+            case 1:
+                break;
+            
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                sair_programa();
+                break;
+
+            default:
+                printf("\nOpção Inválida!\n");
+                printf("Digite a opção novamente!\n\n");
+                break;
+        }
+    }
+    
     return 0;
+}
+
+void exibir_iguais() {
+    for (int i = 0; i < 40; i++) {
+        printf("=");
+    }
+}
+
+void titulo(char *palavra) {
+    exibir_iguais();
+    printf("\n");
+    printf("          ");
+    printf("%s\n", palavra);
+    exibir_iguais();
+}
+
+void menu_inicial() {
+    printf("\n");
+    titulo("ESTAÇÃO DE RECARGA");
+    printf("\n\n");
+    printf("1 - Nova sessão de recarga\n");
+    printf("2 - Listar sessões\n");
+    printf("3 - Buscar sessão\n");
+    printf("4 - Ordenar Sessões\n");
+    printf("5 - Estatísticas\n");
+    printf("6 - Encerrar\n");
+    printf("\n");
+}
+
+void sair_programa() {
+    printf("\n\nPrograma Encerrado!!!\n");
+    exit(EXIT_SUCCESS);
 }
