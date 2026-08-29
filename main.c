1566553432f1207fd7ecd688a6e69297000cce1a
Brunoo7 <brunoolivera072@gmail.com>
2026-08-29 18:47:17 -0300

    case 1 implementa nova sessão de recarga com validações e listagem

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float conversao_para_kwh(float bateria_inicial, float capacidade_bateria) {
+
+    return (bateria_inicial / 100) * capacidade_bateria;
+
+}
+
 typedef struct
 {
     int id;
@@ -20,6 +26,12 @@ void sair_programa();
 int main() {
     int opcao;
 
+    Sessao sessoes[100];
+    int quantidadeSessoes = 0;
+
+    float capacidade_bateria;
+    float energia_necessaria;
+
     menu_inicial();
     
     while (1)
@@ -30,8 +42,56 @@ int main() {
         switch (opcao)
         {
             case 1:
+                printf("\n--- NOVA SESSÃO ---\n");
+
+                sessoes[quantidadeSessoes].id = quantidadeSessoes + 1;
+
+                printf("Sessão: %d\n",
+                       sessoes[quantidadeSessoes].id);
+
+                printf("Digite a capacidade da bateria (kWh): ");
+                scanf("%f", &capacidade_bateria);
+
+                printf("Digite a bateria inicial (%%): ");
+                scanf("%f",
+                      &sessoes[quantidadeSessoes].bateria_inicial);
+
+                if (sessoes[quantidadeSessoes].bateria_inicial < 0 ||
+                    sessoes[quantidadeSessoes].bateria_inicial > 100)
+                {
+                    printf("\nBateria inválida!\n\n");
+                    break;
+                }
+
+                if (capacidade_bateria <= 0)
+                {
+                    printf("\nCapacidade da bateria inválida!\n\n");
+                    break;
+                }
+
+                energia_necessaria =
+                    capacidade_bateria -
+                    conversao_para_kwh(
+                        sessoes[quantidadeSessoes].bateria_inicial,
+                        capacidade_bateria
+                    );
+
+                sessoes[quantidadeSessoes].energia =
+                    energia_necessaria;
+
+                sessoes[quantidadeSessoes].tempo =
+                    (energia_necessaria / 75) * 60;
+
+                sessoes[quantidadeSessoes].custo =
+                    energia_necessaria * 0.8;
+
+                sessoes[quantidadeSessoes].bateria_final = 100;
+
+                quantidadeSessoes++;
+
+                printf("\nSessão cadastrada com sucesso!\n\n");
+
                 break;
-            
             case 2:
                 break;
 
